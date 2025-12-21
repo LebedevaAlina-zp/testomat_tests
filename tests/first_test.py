1743c3ff9481fb07c8052e9b8ccc828bdde0e80d
LebedevaAlina-zp <lebedeva.alina.zp@gmail.ru>
2025-12-21 20:26:24 +0800

    module-2 added dotenv library for configurations added configs fixture with a simple dataclass added Faker library for random data generation

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,53 +1,56 @@
+import pytest
+from faker import Faker
 from playwright.sync_api import Page, expect
 
+TARGET_PROJECT = "Industrial & Jewelry"
 
-def test_sign_in_with_invalid_creds(page: Page):
-    page.goto('https://www.testomat.io')
+from conftest import Config
+
+
+@pytest.fixture(scope="function")
+def login(page: Page, configs: Config):
+    page.goto(configs.login_url)
+    login_user(page, email=configs.email, password=configs.password)
+
+
+def test_sign_in_with_invalid_creds(page: Page, configs: Config):
+    page.goto(configs.base_url)
 
     expect(page.get_by_text('Log in', exact=True)).to_be_visible()
 
     page.get_by_text('Log in', exact=True).click()
+    invalid_password = Faker().password(length=10)
 
-    page.locator("#content-desktop #user_email").fill("lebedeva.alina.zp@gmail.com")
-    page.locator("#content-desktop #user_password").fill("kded;pdq")
+    page.locator("#content-desktop #user_email").fill(configs.email)
+    page.locator("#content-desktop #user_password").fill(invalid_password)
     page.get_by_role("button", name="Sign In").click()
 
     expect(page.locator("#content-desktop .common-flash-info-right")).to_have_text("Invalid Email or password.")
 
 
-def test_search_project_in_company(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
+def test_search_project_in_company(page: Page, configs: Config, login):
+    search_by_project(page, TARGET_PROJECT)
 
-    login_user(page, email="lebedeva.alina.zp@gmail.com", password="ololo")
-
-    target_project = "Industrial & Jewelry"
-
-    search_by_project(page, target_project)
-
-    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
-    expect(page.locator("ul li h3").filter(visible=True)).to_have_text(target_project)
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
+    expect(page.locator("ul li h3").filter(visible=True)).to_have_text(TARGET_PROJECT)
 
 
-def test_should_be_possible_open_free_project(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page, email="lebedeva.alina.zp@gmail.com", password="ololo")
+def test_should_be_possible_open_free_project(page: Page, configs: Config, login):
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
 
     expect(page.get_by_text("Create project")).to_be_visible()
 
-    target_project = "Industrial & Jewelry"
-    search_by_project(page, target_project)
-    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+    search_by_project(page, TARGET_PROJECT)
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_hidden()
 
 
 def search_by_project(page: Page, target_project: str):
-    # expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
     page.locator("#content-desktop #search").fill(target_project)
 
 
-def open_homepage(page: Page):
-    page.goto('https://www.testomat.io')
+def open_homepage(page: Page, configs: Config):
+    page.goto(configs.base_url)
 
 
 def login_user(page: Page, email: str, password: str):
